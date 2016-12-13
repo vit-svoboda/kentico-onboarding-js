213ca1e8a977adaf6620828223a5b9fd44cba0df
zuzanad <zuzanad@kentico.com>
2016-12-13 16:38:32 +0100

    Add app scaffolding (react-create-app)

---
+++ onboarding-task/src/App.jsx
@@ -0,0 +1,39 @@
+import './sticky-footer.css'
+import React, {Component} from 'react';
+import List from './components/List';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="header clearfix">
+                        <h3 className="text-muted">Kentico Academy</h3>
+                    </div>
+
+                    <div className="jumbotron">
+                        <h1>JS onbarding</h1>
+                        <p className="lead">
+                            We will implement simple task using <a href="https://facebook.github.io/react/docs/hello-world.html">ReactJS</a> and later move on to refactor our app to use <a href="https://facebook.github.io/immutable-js/">Immutable</a> and <a href="http://redux.js.org/">Redux</a>.
+                        </p>
+                        <p>You can find all the relevant info in Git repo which also contains theassignment.</p>
+                        <p>
+                            <a className="btn btn-lg btn-success" href="https://github.com/Suzii/kentico-onboarding-js" role="button">Fork me on GitHub</a>
+                        </p>
+                    </div>
+
+                    <section id="app-content">
+                        <div className="row">
+                            <List />
+                        </div>
+                    </section>
+                </div>
+                <footer className="footer">
+                    <p>&copy; 2016 Kentico Software</p>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default App;
