bd45d3de090575db1ba954ef2d5c134b414713c1
zuzanad <zuzanad@kentico.com>
2016-12-13 16:48:47 +0100

    Add eslint and webstorm coding rules fix gitignore

--- onboarding-task/src/components/List.jsx
+++ onboarding-task/src/components/List.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class List extends React.Component {
-    render() {
-        return (
-            <div className="col-sm-12 col-md-offset-2 col-md-8">
-                <pre>// TODO: implement the list there :)</pre>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="col-sm-12 col-md-offset-2 col-md-8">
+        <pre>// TODO: implement the list there :)</pre>
+      </div>
+    );
+  }
 }
 
 export default List;
