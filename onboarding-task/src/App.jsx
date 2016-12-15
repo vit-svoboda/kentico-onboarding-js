b342b68c1b7408cb4d3835ae62e5364b8049321c
Suzii <zuzanad@kentico.com>
2016-12-15 14:46:08 +0100

    Add assignment.gif to App and fix some typos

--- onboarding-task/src/App.jsx
+++ onboarding-task/src/App.jsx
@@ -14,20 +14,16 @@ class App extends Component {
           <div className="jumbotron">
             <h1>JS onbarding</h1>
             <p className="lead">
-              We will implement simple task using
-              <a href="https://facebook.github.io/react/docs/hello-world.html">ReactJS</a> aand later move on to refactor our app to use
-              <a href="https://facebook.github.io/immutable-js/">Immutable</a> and <a href="http://redux.js.org/">Redux</a>.
+              We will implement simple task using <a href="https://facebook.github.io/react/docs/hello-world.html">ReactJS</a> and later move on to refactor our app to use <a href="https://facebook.github.io/immutable-js/">Immutable</a> and <a href="http://redux.js.org/">Redux</a>.
             </p>
-            <p>You can find all the relevant info in Git repo which also contains theassignment.</p>
+            <p>You can find all the relevant info in git repository.</p>
             <p>
               <a className="btn btn-lg btn-success" href="https://github.com/Suzii/kentico-onboarding-js" role="button">Fork me on GitHub</a>
             </p>
           </div>
 
           <section id="app-content">
-            <div className="row">
-              <List />
-            </div>
+            <List />
           </section>
         </div>
         <footer className="footer">
--- onboarding-task/src/components/List.jsx
+++ onboarding-task/src/components/List.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import assignment from './../../../assignment.gif';
 
 class List extends React.Component {
   render() {
     return (
-      <div className="col-sm-12 col-md-offset-2 col-md-8">
-        <pre>// TODO: implement the list there :)</pre>
+      <div className="row">
+        {/* TODO: You can delete the assignment part once you do not need it */}
+        <div className="row">
+          <div className="col-sm-12">
+            <p className="lead text-center">Desired functionality is captured on the gif image. </p>
+            <p className="lead text-center"><b>Note: </b>Try to make solution generic enough so that more list items can be edited at once.</p>
+            <img src={assignment} alt="assignment" className="img--assignment" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12 col-md-offset-2 col-md-8">
+            <pre>
+              // TODO: implement the list here :)
+            </pre>
+          </div>
+        </div>
       </div>
     );
   }
