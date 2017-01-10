1eeb9205c7325bcc71949fb5b243da9eb0d6fa33
radekpetruska <radek.petruska@hotmail.cz>
2017-01-10 12:39:51 +0100

    Fix failing test

--- onboarding-task/src/App.jsx
+++ onboarding-task/src/App.jsx
@@ -1,35 +1,45 @@
 import './sticky-footer.css'
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+import reducer from './reducers/combinedReducer';
 import List from './components/List';
 
 class App extends Component {
   render() {
+    const logger = createLogger();
+    const storeEnhancers = applyMiddleware(logger);
+    const store = createStore(reducer, storeEnhancers);
+
     return (
-      <div>
-        <div className="container">
-          <div className="header clearfix">
-            <h3 className="text-muted">Kentico Academy</h3>
-          </div>
+      <Provider store={store}>
+        <div>
+          <div className="container">
+            <div className="header clearfix">
+              <h3 className="text-muted">Kentico Academy</h3>
+            </div>
 
-          <div className="jumbotron">
-            <h1>JS onboarding</h1>
-            <p className="lead">
-              We will implement simple task using <a href="https://facebook.github.io/react/docs/hello-world.html">ReactJS</a> and later move on to refactor our app to use <a href="https://facebook.github.io/immutable-js/">Immutable</a> and <a href="http://redux.js.org/">Redux</a>.
-            </p>
-            <p>You can find all the relevant info in git repository.</p>
-            <p>
-              <a className="btn btn-lg btn-success" href="https://github.com/Suzii/kentico-onboarding-js" role="button">Fork me on GitHub</a>
-            </p>
-          </div>
+            <div className="jumbotron">
+              <h1>JS onboarding</h1>
+              <p className="lead">
+                We will implement simple task using <a href="https://facebook.github.io/react/docs/hello-world.html">ReactJS</a> and later move on to refactor our app to use <a href="https://facebook.github.io/immutable-js/">Immutable</a> and <a href="http://redux.js.org/">Redux</a>.
+              </p>
+              <p>You can find all the relevant info in git repository.</p>
+              <p>
+                <a className="btn btn-lg btn-success" href="https://github.com/Suzii/kentico-onboarding-js" role="button">Fork me on GitHub</a>
+              </p>
+            </div>
 
-          <section id="app-content">
-            <List />
-          </section>
+            <section id="app-content">
+              <List />
+            </section>
+          </div>
+          <footer className="footer">
+            <p>&copy; 2016 Kentico Software</p>
+          </footer>
         </div>
-        <footer className="footer">
-          <p>&copy; 2016 Kentico Software</p>
-        </footer>
-      </div>
+      </Provider>
     );
   }
 }
--- onboarding-task/src/index.js
+++ onboarding-task/src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import createLogger from 'redux-logger';
-import reducer from './reducers/combinedReducer';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-const logger = createLogger();
-const storeEnhancers = applyMiddleware(logger);
-const store = createStore(reducer, storeEnhancers);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('app-root')
 );
